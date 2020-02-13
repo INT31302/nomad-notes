14afe64cedd51f2974d4f115956532144d148f31
INT31302 <tkdwo287@gmail.com>
2020-02-13 19:42:14 +0900

    add mutation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import { GET_NOTES } from "./queries";
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Query query={GET_NOTES}>{() => null}</Query>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/clientState.js
+++ src/clientState.js
@@ -1,19 +1,29 @@
+import { NOTE_FRAGMENT } from "./fragments";
+import { GET_NOTES } from "./queries";
+
 export const defaults = {
-  note: []
+  notes: [
+    {
+      __typename: "Note",
+      id: 1,
+      title: "First",
+      content: "Second"
+    }
+  ]
 };
 export const typeDefs = [
   `
     schema{
         query:Query
-        mutaion:Mutaion
+        mutation:Mutation
     }
     type Query{
         notes: [Note]!
         note(id: Int!): Note
     }
     type Mutation{
-        createNote(title: String!, content: String!)
-        editNote(id: String!, title:String!, content:String!)
+        createNote(title: String!, content: String!):Note
+        editNote(id: String!, title:String!, content:String!):Note
     }
     type Note{
         id: Int!
@@ -24,6 +34,31 @@ export const typeDefs = [
 ];
 export const resolvers = {
   Query: {
-    notes: () => []
+    note: (_, variables, { cache }) => {
+      const id = cache.config.dataIdFromObject({
+        __typename: "Note",
+        id: variables.id
+      });
+      const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id });
+      return note;
+    }
+  },
+  Mutation: {
+    createNote: (_, variables, { cache }) => {
+      const { notes } = cache.readQuery({ query: GET_NOTES });
+      const { title, content } = variables;
+      const newNote = {
+        __typename: "Note",
+        title,
+        content,
+        id: notes.length + 1
+      };
+      cache.writeData({
+        data: {
+          notes: [newNote, ...notes]
+        }
+      });
+      return newNote;
+    }
   }
 };
